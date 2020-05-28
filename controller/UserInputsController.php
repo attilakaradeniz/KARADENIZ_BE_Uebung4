ce6b8604bf20c40bb0781a77ad09a7443d8e8acb
attilakaradeniz <attilakaradeniz@gmail.com>
2020-05-29 00:25:28 +0200

    dan vers std class & ...

--- controller/UserInputsController.php
+++ controller/UserInputsController.php
@@ -23,7 +23,10 @@ class UserInputsController
             $_SESSION['shoppingCart'] = array();
         }
 
-        $shoppingCart = ['shoppingCart' =>  $_SESSION['shoppingCart']];
+        $shoppingCart = new ShoppingCartModel();
+
+
+        //$shoppingCart = ['shoppingCart' =>  $_SESSION['shoppingCart']];
 
         if (isset($_GET['action'])) {
             //$this->action = $_GET['action'];
@@ -34,32 +37,7 @@ class UserInputsController
                 case 'listTypes' :
                     $modelTypes = new StoreTypesModel();
                     $this->lisTypesData = $db->queryData("SELECT id, name FROM  product_types ORDER BY name");
-                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                    //$modelTypes->tableData($this->lisTypesData);
-//                    echo "\n";
-//                    echo "\n";
-//                    //print_r($this->lisTypesData);  // TEST to see the data
-//                    $bosArray = array();
-//                foreach($this->lisTypesData as $item){
-//                    $bosArray[] = $item;
-//                    print_r($item);
-//                    echo '<pre>';
-//                }
-//
-//                    $testToSee = array( 'product' => 'cream', 'tip' => 'vicik', 'price' => 'coookcok');
-//                    $JSON_testToSee = json_encode($testToSee);
-//                    $testToSee = "[".$testToSee."]";
-//
-//                    $this->view->output($testToSee);
-//
-//                    //$this->view->output($JSON_testToSee);
-//
-//                    //print_r($testToSee);
-//                    //echo $testToSee;
-//
-//                    echo "\n";
-//                    echo "\n";
-                    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    ///////////////////////
                     $data = $modelTypes->fetchTypesData($this->lisTypesData);
                     $this->view->output($data);
 
@@ -79,34 +57,39 @@ class UserInputsController
                     break;
 
                 case 'listCart' :
-                    echo 'you have chosen listCart'; // TEST
-
+                    // echo 'you have chosen listCart'; // TEST
+                    $this->view->output($_SESSION);
                     break;
 
                 case 'addArticle' :
                     // echo 'you have chosen addArticle'; // TEST
                     $this->articleId = filter_input(INPUT_GET, "articleId", FILTER_SANITIZE_STRING);
                     //$statement = "SELECT * FROM products WHERE id=119;"; // TEST
+                    // array_push($_SESSION['shoppingCart'], ['articleName' => $data, 'amount' => 1]);
 
-                    $statement = "SELECT * FROM products WHERE id=" . $this->articleId;
-//                    $data = $db->queryData($statement);
-                    $data = $db->queryData($statement)[0]['name'];
-
-
-                    //$_SESSION['shoppingCart'][] = $data; // $_SESSION'a $data 'yı ekliyor
-
-                // Burada yapılan işlem list cart a ekleyip list cart'ın gösterilmesi
-                    array_push($_SESSION['shoppingCart'], ['articleName' => $data, 'amount' => 1]);
-
+                    $shoppingCart->addToCart($this->articleId);
                     $this->view->output($_SESSION);
                     //$this->view->output($data);
                     //print_r($_SESSION['shoppingCart']);
-                    // session_destroy(); // --------------------- IF ANYTHING GOES WRONG USE THIS ----------------------- /
+                    // session_destroy(); // ------------------------------------------- IF ANYTHING GOES WRONG USE THIS ------------------------------------ /
+                    // -------------------------------------------------------------------DONT FORGET TO DISABLE --------------------------------------------/
                     break;
 
                 case 'removeArticle' :
-                    echo 'you have chosen removeArticle'; // TEST
+                    // echo 'you have chosen removeArticle'; // TEST
+                    //print_r($shoppingCart->getProductName(119)); // TEST
+                    $this->articleId = filter_input(INPUT_GET, "articleId", FILTER_SANITIZE_STRING);
 
+                    $shoppingCart->removeFromCart($this->articleId);
+
+                    break;
+
+                case 'articleNameById' : // TEST
+                    $this->articleId = filter_input(INPUT_GET, "articleId", FILTER_SANITIZE_STRING);
+                    //print_r($shoppingCart->getProductName($this->articleId));
+                    //$this->view->output($shoppingCart->getProductName($this->articleId)); // This brings it as json format
+                    //print_r($shoppingCart->getProductName($this->articleId)); // this brings it as array format
+                    print_r($shoppingCart->getProductName($this->articleId)[0]['name']); // this grabs only at 0th position key:name's value , means just name as string format
                     break;
 
                 default:
--- model/ShoppingCartModel.php
+++ model/ShoppingCartModel.php
@@ -2,64 +2,169 @@
 
 class ShoppingCartModel
 {
-    private $shoppingCart;
-    private $productsArray;
-    private $productsDB;
-    private $amount;
-    private $statementProducts;
-    private $articleId;
+    //private $shoppingCartArray;
+    private $productName;
 
-    public  function __construct()
+//    public function getShoppingCart()
+//    {
+//        $_SESSION['shoppingCart'] = $this->shoppingCartArray;
+//        return $this->shoppingCartArray;
+//    }
+
+
+    public function getProductName($id)
     {
+        $statement = "SELECT name FROM products WHERE id = $id";
+        $db = new DatabaseService();
+        $db->connect();
 
-        $this->productsDB = new DatabaseService();
-        $this->productsDB->connect();
-        $this->statementProducts =
-        $this->productsDB->queryData();
+        return $this->productName = $db->queryData($statement);
 
-
-
+        //$this->productName = $db->queryData($statement)[0]['name'];
     }
 
-
-    /**
- * @return mixed
- */
-    public function getShoppingCart()
+    public function addToCart($id)
     {
-        $_SESSION['shoppingCart'] = $this->shoppingCart;
-        return $this->shoppingCart;
-    }
+        $currentCart = $_SESSION['shoppingCart'];
 
+        if ($this->isInCart($id)){
+            foreach ($_SESSION['shoppingCart'] as $item) {
+                if($item->id == $id){
 
+                    $item->amount++;
+                }
 
-    public function addToCart(){
-
-        // Session
-        if (!isset($_SESSION['shoppingCart'])) {
-            $_SESSION['shoppingCart'] = array();
+            }
         }
-//        $shoppingCart = ['shoppingCart' =>  $_SESSION['shoppingCart']];
+        else {
 
-        $_SESSION['shoppingCart'][] = $this->shoppingCart;
+            $statement = "SELECT name, id, price_of_sale as price FROM products WHERE id=" . $id;
+            $db = new DatabaseService();
+            $db->connect();
+            $data = $db->queryData($statement);
+            //print_r($data);
+            //$temp = $this->getProductName($id)[0]['name'];
+
+            $article = new stdClass();
+
+            $article->productName = $data[0]['name'];
+            $article->id = $data[0]['id'];
+            $article->price = $data[0]['price'];
+
+            $article->amount = 1;
+
+            print_r($article);
+
+            $_SESSION['shoppingCart'][] = $article;
+
+            //$_SESSION['shoppingCart'][] = ['articleName' => $temp, 'amount' => 1];
+
+
+        }
+
+
+        //$_SESSION['shoppingCart'][] = $data;
+
+
+
+        //$_SESSION['shoppingCart'][] = ['articleName' => $this->productName[0]["name"]];
+
+
+
+
+
+//        if($_SESSION['shoppingCart'] == ''){
+//
+//        }
+//        foreach ($_SESSION['shoppingCart'] as $key => $value) {
+//
+//
+//
+//             if($value["articleName"] == $temp && $value['amount'] == 1){
+//                $amountPlus = $value["amount"] + 1;
+//                $_SESSION['shoppingCart'][$key]["amount"] = $amountPlus;
+//            }
+//
+//
+//
+//        }
+
+
+        //array_push($_SESSION['shoppingCart'], ["articleName" => $this->productName[0]['name']]);
+        //$shoppingCartArray = ['shoppingCart' => $_SESSION['shoppingCart']];
+
+        //$_SESSION['shoppingCart'][] = $this->shoppingCartArray;
+    }
+
+
+    public function isInCart($id)
+    {
+        $currentCart = $_SESSION['shoppingCart'];
+        foreach ($currentCart as $item) {
+
+            if($item->id == $id){
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+
+    public function removeFromCart($id)
+    {
+        $currentCart = $_SESSION['shoppingCart'];
+
+     if($this->isInCart($id)){
+        foreach ($currentCart as $item) {
+          if($item->id == $id){
+              $item->amount--;
+
+          }
+
+        }
+     }
+
+     $currentCart = $this->removeEmptyArticles($id, $currentCart);
+     $_SESSION['shoppingCart'] = $currentCart;
+
+    }
+
+    public function removeEmptyArticles($id, $currentCart){
+        //$currentCart = $_SESSION['shoppingCart'];
+
+        foreach ($currentCart as $item => $object) {
+            if($object->amount == 0){
+                unset($currentCart[$item]);
+
+            }
+
+        }
+
+        return $currentCart;
+
     }
 
 
 
-    public function removeFromCart(){
+
+
+
+
+    public function listCart()
+    {
 
     }
 
-
-    public function listCart(){
-
-    }
-
-    public function testProduct(){
+    public function testProduct()
+    {
 
     }
 
-    public function fetchProductData($data){
+    public function fetchProductData($data)
+    {
         $product = [];
     }
 
